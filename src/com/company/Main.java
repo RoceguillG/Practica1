4fbcbbd9edcec6bb133a7d52f5c60dacc31bb86a
Rocio <guisell.alvarado@gmail.com>
2021-08-21 18:19:20 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//menu inicial
+//inicio del menu
+Scanner opcion = false;
+boolean salir=false;
+
+        i=0;
+        while () {
+        }
+
+
+
+    }
+}
