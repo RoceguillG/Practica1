f4633afb8bc79f093080c9395042529407e2c7ac
Rocio <guisell.alvarado@gmail.com>
2021-08-23 17:53:29 -0600

    el uso de try catch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,17 @@
 package com.company;
 
-import java.sql.SQLOutput;
+import jdk.swing.interop.SwingInterOpUtils;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	//menu inicial
+        //menu inicial
 //inicio del menu
         Scanner entrada = new Scanner(System.in);
         boolean salir = false;
-        int opcion = 0;
+        int opcion;
 
 
         do {
@@ -21,16 +22,48 @@ public class Main {
             System.out.println(" 2. Renaudar Juego ");
             System.out.println(" 3. Generar Reportes ");
             System.out.println(" 4. Salir ");
-            System.out.println(" Ingrese la opción que desea realizar: ");
-            opcion = entrada.nextInt();
+
+
+            try {
+                System.out.println(" Ingrese la opción que desea realizar: ");
+                opcion = entrada.nextInt();
 
 //estructura del Switch para realizar las operaciones
 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("INIO DELL JEUGO  ");
 
 
+                        break;
+                    case 2:
+                        System.out.println("RENAUDAR JUEGO");
 
-        }while (opcion!=4);
+                        break;
+                    case 3:
+                        System.out.println("REPORTES");
+                        break;
+                    case 4:
 
+                        salir = true;
+
+                        System.out.println("SALIR DEL JUEGO");
+
+                        System.out.println("Has salido exitosamente del juego!");
+                        break;
+
+                    default:
+                        System.out.println("Ha ingresado una opción incorrecta");
+                        break;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("¡¡¡Debes insertar un número!!!");
+               // utilizar el nombre de la variable de entrada, y solo .next para que jale bien el trycatch
+                entrada.next();
+
+            }
+        } while (!salir);
 
 
     }
